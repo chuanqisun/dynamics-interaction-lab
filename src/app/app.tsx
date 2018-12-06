9e23d5900405ad00c72fedc69032d57848707795
Chuánqí Sun <chusun@microsoft.com>
2018-12-05 16:00:05 -0800

    clean up comments

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,21 +1,13 @@
 import * as React from "react";
 import {Suspense, lazy} from "react";
 import {Route, BrowserRouter, NavLink} from 'react-router-dom'
-// import {} -> name import -> import some parts of the module, invented in es6
-// import * -> autonamous import -> import entire module 
-// require -> old world from node.js development
-// so new world use export default Class not module.exports anymore
-// module.exports -> we can't do name import we can just only do import *, but if we do export default class we can import name
 
 const Demos = lazy(() => import('./demos'));
 const Experiments = lazy(() => import('./experiments'));
 
 export class App extends React.Component<any, any> {
-  // <any, any> -> type script requires you to declare the property and states 
   constructor(props: any) {
-    // interit props from parent components.
     super(props);
-    // call the constructor of the parent to extend or do some activity to your class.
     
   }
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,11 @@ const path = require('path');
 
 module.exports = {
   mode: "development",
-  devtool: 'inline-source-map', // compile js into main.js bundle, sourcemap = map the single file as bundle to the original javascript code
+  devtool: 'inline-source-map',
   entry: "./src/index.tsx", 
   module: {
     rules: [
       { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ }, 
-      // match the string by look at the file name if it matches with regex of test -> it will use ts-loader to transpile
-      // compile = turn not machine readable to what machine readable
-      // transpile = change the level of spec, but the output still run by browser
       { test: /\.css/, use: ['style-loader', 'css-loader']},
       { test: /\.(ttf|eot|woff|woff2)$/, use: "file-loader"},
     ]
@@ -22,10 +19,6 @@ module.exports = {
     path: path.resolve(__dirname, "./dist"),
     publicPath: '/dist/',
   },
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
   externals: {
     "react": "React",
     "react-dom": "ReactDOM",
