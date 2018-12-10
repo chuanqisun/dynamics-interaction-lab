09b262c82ab2d9ed42ca91099b0e77474effeef2
Chuánqí Sun <chusun@microsoft.com>
2018-12-10 10:06:18 -0800

    add form tabs wip

--- src/components/form-tabs/form-tabs.demo.tsx
+++ src/components/form-tabs/form-tabs.demo.tsx
@@ -1,20 +1,36 @@
 import * as React from 'React';
 import './form-tabs.css';
 
+const tabs = [
+  {id: '0', name: 'Summary'},
+  {id: '1', name: 'Relationship Analytics'},
+  {id: '2', name: 'Product line items'},
+  {id: '3', name: 'Quotes'},
+  {id: '4', name: 'Related'},
+];
+
 export class FormTabsDemo extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      selectedTabId: '0',
+    };
+  } 
+
   render() {
     return <React.Fragment>
     <section>
       <h2 className="lab-demo-h2">Sample</h2>
-      <ul>
-        <li className="tab tab--selected">Summary</li>
-        <li className="tab">Relationship Analytics</li>
-        <li className="tab">Product line items</li>
-        <li className="tab">Quotes</li>
-        <li className="tab">Related</li>
-      </ul>
+      <div>
+        {tabs.map(tab => <button key={tab.id} className={`tab${tab.id === this.state.selectedTabId ? ' tab--selected' : ''}`} onClick={() => this.onSelect(tab.id)}>{tab.name}</button>)}
+      </div>
     </section>
     </React.Fragment>
+  }
+
+  onSelect = (id: string) => {
+    this.setState({selectedTabId: id});
   }
 }
 
